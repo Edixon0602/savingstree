509e0daa5921b3056d8469f669b6bbaf9f765649
Edixon0602 <e.serrano0602@gmail.com>
2024-05-06 08:57:12 -0400

    Add: new trees

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -9,17 +9,20 @@ const TREES = {
   0 : "src/assets/models/small-tree.glb",
   100 : "src/assets/models/small-tree-not-dry.glb",
   200 : "src/assets/models/stylized_tree.glb",
+  300 : "src/assets/models/oak_tree.glb",
+  400 : "src/assets/models/tree_gn.glb",
+  500 : "src/assets/models/mango_tree.glb",
+  600 : "src/assets/models/japanese_maple_tree.glb",
 }
-
-
 
 export default function Tree({addingSavings, withdrawingSavings}) {
   const [tree, setTree] = useState(null);
   useEffect(() => {
     const savedAmount = db.getData("savings");
     for (const key in TREES) {
-      if (key <= savedAmount) {
+      if (savedAmount <= key) {
         setTree(TREES[key]);
+        break;
       }
     }
   }, [addingSavings, withdrawingSavings]);
